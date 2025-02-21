4da06165d93c12f8219c563078f14510bda1c386
Rohith Jayawardene <gambol99@gmail.com>
2025-02-21 17:54:58 +0000

    chore: moving to dev branch

--- scripts/make-dev.sh
+++ scripts/make-dev.sh
@@ -12,16 +12,14 @@ setup_cluster() {
   echo "Provisioning cluster: ${cluster_name}"
   # Create cluster (ignore error if exists)
   kind create cluster --name "${cluster_name}" 2>/dev/null || true
-  # Create ArgoCD namespace (ignore error if exists)
+  # Create the namespace 
   kubectl create namespace argocd --context "${cluster_context}" 2>/dev/null || true
-  # Install ArgoCD
-  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml --context "${cluster_context}" > /dev/null
   # Apply the tenant configuration for the specific cluster
   echo "Applying tenant configuration for ${cluster_name}"
   ## Create a symlink to the kustomize directory
   cp clusters/${cluster_name}.yaml kustomize/overlays/${cluster_name}/platform.yaml
   # Apply the tenant configuration for the specific cluster
-  kubectl apply -k kustomize/overlays/${cluster_name} --context "${cluster_context}"
+  kubectl kustomize kustomize/overlays/${cluster_name} --enable-helm=true --context "${cluster_context}" | kubectl apply -f -
 }
 
 # Main execution
