1e00c8560343b6671f04be838861b464f0c9337e
Rohith Jayawardene <gambol99@gmail.com>
2025-03-06 14:21:50 +0000

    feat: switching to using auto mode

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,8 @@ module "eks" {
   cluster_enabled_log_types      = null
   cluster_endpoint_public_access = var.cluster_endpoint_public_access
   cluster_name                   = var.cluster_name
+  eks_managed_node_groups        = {}
+  enable_auto_mode               = true
   enable_nat_gateway             = var.enable_nat_gateway
   nat_gateway_mode               = var.nat_gateway_mode
   private_subnet_netmask         = var.private_subnet_netmask
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,9 +5,9 @@ provider "aws" {}
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
     }
